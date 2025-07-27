8830ec7a08b5009891ddeccd911bd059665f7e9a
IsaacNgRWI <isaacngrwi@gmail.com>
2025-07-27 22:41:43 +0800

    Added function to set background for video

--- composition.py
+++ composition.py
@@ -1,6 +1,11 @@
 from moviepy import *
 
 class Composition:
-    def __init__(self, elm_list, bgm):
+    def __init__(self, bg, elm_list, bgm):
+        self.bg = bg
         self.elm_list = elm_list
         self.bgm = bgm
+
+    def set_bg(self, bg = None):
+        if bg is None:
+            bg = self.bg
