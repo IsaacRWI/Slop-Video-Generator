4064f7d73cb86d02e724919b8813aeb3cf84ab1e
IsaacNgRWI <isaacngrwi@gmail.com>
2025-08-16 19:15:33 +0800

    Added tweepy api initiation

--- twitter_scraper.py
+++ twitter_scraper.py
@@ -6,3 +6,4 @@ load_dotenv()
 auth = tweepy.OAuth1UserHandler(
     os.getenv("consumer_key"), os.getenv("consumer_secrets"), os.getenv("access_token"), os.getenv("access_token_secret")
 )
+api = tweepy.API(auth, wait_on_rate_limit=True)
