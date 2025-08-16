ea1f406fdc4c339bf5da6b07b75b6192ef20946e
IsaacNgRWI <isaacngrwi@gmail.com>
2025-08-16 19:47:53 +0800

    Added function to search twitter for tweets with certain keywords and filters - Added search function that uses tweepy api search function to retrieve tweets with certain keywords excluding retweets, replies anf links - Added pandas container to store data retrieved from api - Search function has not been tested and does not work yet due to status fail 403 Forbidden (access level not high enough)

--- testing.py
+++ testing.py
@@ -1,8 +1,12 @@
 from composition import *
+import os
+import twitter_scraper
 video1 = Composition("saxophone.mp4", ["element_sample.png", "element_sample.png"], "Invitation to Dance.mp3")
 video1.get_properties()
 print(video1.bgm)
 # video1.set_bgm("End of the Line.mp3")
 # print(video1.bgm)
 # video1.gen_video()
-gen_sample()
+# gen_sample()
+
+twitter_scraper.search()
--- twitter_scraper.py
+++ twitter_scraper.py
@@ -1,9 +1,22 @@
 import tweepy
 from dotenv import load_dotenv
 import os
+import pandas as pd
 
 load_dotenv()
 auth = tweepy.OAuth1UserHandler(
     os.getenv("consumer_key"), os.getenv("consumer_secret"), os.getenv("access_token"), os.getenv("access_token_secret")
 )
 api = tweepy.API(auth, wait_on_rate_limit=True)
+
+query = "'Elon Musk' 'Announced' -filter:retweets AND -filter:replies AND -filter:links"
+number = 2
+
+def search():
+    try:
+        tweets = api.search_tweets(q=query, lang="en", count=number, tweet_mode = "extended")
+        attribute_container = [[tweet.user.name, tweet.created_at, tweet.favorite_count, tweet.source, tweet.full_text] for tweet in tweets]
+        columns = ["User", "Date Created", "Likes", "Source", "Content"]
+        tweets_df = pd.DataFrame(attribute_container, columns=columns)
+    except Exception as e:
+        print(f"Status failed on {e}")
